50d172b3f80a1d9ae2dc6d8cde911d5bc292d94c
GSWXXN <819269088@qq.com>
2021-09-22 09:24:52 +0800

    update

--- sleep.py
+++
@@ -1,8 +0,0 @@
-import random
-import time
-
-
-if __name__ == '__main__':
-    start_aft = random.randint(1800, 10800)
-    print("Start \"Click 315\" after " + str(start_aft) + " second")
-    time.sleep(start_aft)
